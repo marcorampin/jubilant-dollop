682963b4b5eb2ae0c3f0b907ba96f025a5785a3f
Marco Rampin <71671995+marcorampin@users.noreply.github.com>
2023-10-21 21:14:59 +0200

    Add files via upload

--- desktop_scripts/random-wallpaper.sh
+++ desktop_scripts/random-wallpaper.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
 
+IFS="
+"
+wallpaper_directory=$1
+duration=$2
+
+while true; do
+	ls $wallpaper_directory |sort -R |tail -$N |while read file; do
+   	current_swaybg_pid=$(pgrep -x swaybg)
+   	wallpaper="$wallpaper_directory/$file"
+   	sh -c "swaybg -o \"*\" -i $wallpaper -m fill -c \"#000000\" > /dev/null 2>&1 &" \
+       	&& sleep 1 \
+       	&& kill $current_swaybg_pid
+   	sleep $duration
+	done
+done
